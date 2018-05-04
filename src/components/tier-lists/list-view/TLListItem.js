5cc92caedd186f4b2824a21aa7a49fae8724b5c4
jkhoang313 <jkhoang313@gmail.com>
2018-05-03 21:06:03 -0400

    Added minor styling to tier list list items

--- src/components/tier-lists/list-view/TLListItem.js
+++ src/components/tier-lists/list-view/TLListItem.js
@@ -4,6 +4,11 @@ export default class TLListItem extends Component {
   render() {
     const { tierList } = this.props;
 
-    return <div>{tierList.get("title")}</div>;
+    return (
+      <div className="tl-list-item_container">
+        <div className="tl-list-item_header">{tierList.get("title")}</div>
+        <div className="tl-list-item_body">tier list body</div>
+      </div>
+    );
   }
 }
